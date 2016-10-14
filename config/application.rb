2015506350d1c5e13668cd2354425ac9ae050afc
Virginia <vcdooley@gmail.com>
2016-10-14 06:25:54 +0100

    updated application.rb in hopes of images appearing

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Theriseapp
     # Enable the asset pipeline
     config.assets.enabled = true
     config.serve_static_assets = true
+    config.serve_static_files = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
