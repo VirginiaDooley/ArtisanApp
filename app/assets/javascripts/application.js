5a976524f729dee02f2b595e6bc84357750a3fb4
Virginia <vcdooley@gmail.com>
2017-09-19 10:34:22 +0100

    removed bootstrap requirements from .js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
-//= require bootstrap
 //= require jquery_ujs
 //= require masonry/jquery.masonry
 //= require turbolinks
